46e9b8ac0d749250cc8d63975c9f3499f68b5dfd
jianghua2 <jianghua2@100tal.com>
2018-10-23 11:03:25 +0800

    a

--- src/chapter_2_listproblem_self/Problem_07_IsPalindromeList.java
+++ src/chapter_2_listproblem_self/Problem_07_IsPalindromeList.java
@@ -61,6 +61,7 @@ public class Problem_07_IsPalindromeList {
 
 
 
+    //todo
     public static boolean isPalindrome3(Node head) {
         if(head == null || head.next == null) {
             return true;
@@ -76,7 +77,7 @@ public class Problem_07_IsPalindromeList {
         Node left2 = slow.next;
         Node right1 = slow;
         //翻转链表
-        Node pre = right1;
+        Node pre = null;
         Node next = null;
         Node cur = left2;
         while (cur!=null){
@@ -85,7 +86,7 @@ public class Problem_07_IsPalindromeList {
             pre = cur;
             cur = next;
         }
-
+        printLinkedList(head);
         boolean res = true;
         Node cur1 = head;
         cur = pre;
@@ -98,6 +99,7 @@ public class Problem_07_IsPalindromeList {
             cur = cur.next;
         }
 
+
         //恢复
         pre = null;
         next = null;
@@ -109,11 +111,54 @@ public class Problem_07_IsPalindromeList {
             cur = next;
         }
         right1.next = pre;
+        printLinkedList(head);
 
 
 
         return res;
 
+    }
+
+    // need O(1) extra space
+    public static boolean isPalindrome4(Node head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        Node n1 = head;
+        Node n2 = head;
+        while (n2.next != null && n2.next.next != null) { // find mid node
+            n1 = n1.next; // n1 -> mid
+            n2 = n2.next.next; // n2 -> end
+        }
+        n2 = n1.next; // n2 -> right part first node
+        n1.next = null; // mid.next -> null
+        Node n3 = null;
+        while (n2 != null) { // right part convert
+            n3 = n2.next; // n3 -> save next node
+            n2.next = n1; // next of right node convert
+            n1 = n2; // n1 move
+            n2 = n3; // n2 move
+        }
+        n3 = n1; // n3 -> save last node
+        n2 = head;// n2 -> left first node
+        boolean res = true;
+        while (n1 != null && n2 != null) { // check palindrome
+            if (n1.value != n2.value) {
+                res = false;
+                break;
+            }
+            n1 = n1.next; // left to mid
+            n2 = n2.next; // right to mid
+        }
+        n1 = n3.next;
+        n3.next = null;
+        while (n1 != null) { // recover list
+            n2 = n1.next;
+            n1.next = n3;
+            n3 = n1;
+            n1 = n2;
+        }
+        return res;
     }
 
 
