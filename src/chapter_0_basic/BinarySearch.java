9ec7f02915c4e8603445f47f1b2a6295093dcafa
jianghua2 <jianghua2@100tal.com>
2018-11-05 16:06:53 +0800

    练习：二分查找

--- src/chapter_0_basic/BinarySearch.java
+++ src/chapter_0_basic/BinarySearch.java
@@ -13,8 +13,19 @@ public class BinarySearch {
         int left = 0;
         int right = arr.length-1;
         int mid;
+
+        //为什么 while 里的条件是 <，而不是 <=。
+        // 一方面是我们想在循环外部判断最终的 left 位置是否是目标值，
+        // 另一方面是如果循环条件允许 left = right，那么最后 mid = left = right，如果该处正好是目标值，那么 right 将始终等于 mid，不会再左移，就会陷入死循环。
         while (left < right){
             mid = (left+right)/2;
+            //当中间值小于目标元素的时候，目标元素在序列的右边：left = temp + 1；
+            /**
+             * 其余的情况目标值在序列的左边：right = temp；
+             * 我们要查找的第一个目标元素的位置，一般的情况就是目标元素存在多个情况，由上述的两个判断条件，我们可以知道，如果查找到了目标元素，并且该目标元素不是第一个的时候，
+             * 此时left<right，判断继续进行。我们这里设置的判断条件保证了，当中间值小于目标元素的时候，left一定会向右移动，
+             * 同时如果中间值恰好等于目标元素的时候，right能够不动，最终序列结束的时候，left=right=目标元素的第一次出现的位置！
+             */
             if(arr[mid]<target){
                 left = mid+1;
             }else {
---
+++ src/chapter_0_basic/BinarySearchForARange.java
@@ -0,0 +1,9 @@
+package chapter_0_basic;
+
+/**
+ * @author Jianghua
+ * 2018年11月05日  15：41
+ */
+public class BinarySearchForARange {
+
+}
