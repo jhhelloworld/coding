126253514eff81573149d1b15921de8c57e6e80b
jhhelloworld <jh_helloworld@163.com>
2019-05-05 22:54:37 +0800

    并查集结构

--- src/zcy_lesson/code_05/Code_04_UnionFind.java
+++ src/zcy_lesson/code_05/Code_04_UnionFind.java
@@ -3,6 +3,9 @@ package zcy_lesson.code_05;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * 并查集问题
+ */
 public class     Code_04_UnionFind {
 
 	public static class Node {
@@ -10,23 +13,36 @@ public class     Code_04_UnionFind {
 	}
 
 	public static class UnionFindSet {
+		//key :child  value:father
 		public HashMap<Node, Node> fatherMap;
+		//某个节点所在的集合有多少个节点
 		public HashMap<Node, Integer> sizeMap;
 
 		public UnionFindSet() {
+
 			fatherMap = new HashMap<Node, Node>();
 			sizeMap = new HashMap<Node, Integer>();
 		}
 
+		/**
+		 * 初始化
+		 * @param nodes
+		 */
 		public void makeSets(List<Node> nodes) {
 			fatherMap.clear();
 			sizeMap.clear();
 			for (Node node : nodes) {
+				//开始每个node自己成为一个集合
 				fatherMap.put(node, node);
 				sizeMap.put(node, 1);
 			}
 		}
 
+		/**
+		 * 找代表节点，期间扁平化处理  递归处理
+		 * @param node
+		 * @return
+		 */
 		private Node findHead(Node node) {
 			Node father = fatherMap.get(node);
 			if (father != node) {
