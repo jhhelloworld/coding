eca3406a04e0c22f2954cf0a46cea9f7e54fd4c3
jianghua2 <jianghua2@100tal.com>
2019-05-16 14:37:21 +0800

    添加工具类 TreeNode

---
+++ src/util/TreeNode.java
@@ -0,0 +1,15 @@
+package util;
+
+/**
+ * jh
+ * 2019年05月16日  10：18
+ */
+public class TreeNode {
+    public TreeNode left;
+    public TreeNode right;
+    public int val;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
