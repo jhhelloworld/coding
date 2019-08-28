f3caef5171f5c14bf29b17a93e4df3dec0449c7e
jhhelloworld <jh_helloworld@163.com>
2019-08-29 00:27:36 +0800

    二叉树的层次遍历

---
+++ src/zcy_book_v2/chapter_3_binarytreeproblem/Q_103_BinaryTreeZigzagLevelOrderTtraversal.java
@@ -0,0 +1,19 @@
+package zcy_book_v2.chapter_3_binarytreeproblem;
+
+import util.TreeNode;
+
+import java.util.List;
+
+/**
+ * jh
+ * 2019年08月29日  00：12
+ * 二叉树的锯齿形层次遍历
+ */
+public class Q_103_BinaryTreeZigzagLevelOrderTtraversal {
+    //思路：遍历当前层，把下一层节点放双端队列
+    //当前层从左到右：把
+    //flag ：下一层从左到右，当先放当前层的
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+    }
+}
