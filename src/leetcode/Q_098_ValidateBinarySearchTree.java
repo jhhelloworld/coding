f8eb1f391f0477a317b3b728b0f78330ee477fc2
jianghua2 <jianghua2@100tal.com>
2019-08-28 10:34:58 +0800

    验证二叉搜索树

---
+++ src/leetcode/Q_098_ValidateBinarySearchTree.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+import a_test.Main;
+import basic.binarytree.Practice;
+import util.TreeNode;
+
+/**
+ * jh
+ * 2019年08月27日  21：08
+ *
+ * 验证二叉搜索树
+ *
+ *
+ */
+public class Q_098_ValidateBinarySearchTree {
+
+    public boolean isValidBST(TreeNode root) {
+        return root == null || process(root,Long.MAX_VALUE,Long.MIN_VALUE);
+    }
+
+    /**
+     *
+     * @param root 当前节点
+     * @param maxValue  当前节点可能的最大值
+     * @param minVlue 当前节点可能的最小值
+     * @return
+     */
+    public boolean process(TreeNode root ,long maxValue,long minVlue){
+        if(root == null){
+            return true;
+        }
+        if(root.val>=maxValue || root.val<=minVlue){
+            return false;
+        }
+        return process(root.left,root.val,minVlue) && process(root.right,maxValue,root.val);
+    }
+
+
+
+
+}
