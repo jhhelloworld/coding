cd6606cc3e46466bebea5e8c131e519d94cbdc20
jianghua2 <jianghua2@100tal.com>
2019-08-28 15:26:23 +0800

    加一

--- src/leetcode/Q_066_PlusOne.java
+++ src/leetcode/Q_066_PlusOne.java
@@ -5,6 +5,13 @@ import java.beans.beancontext.BeanContext;
 /**
  * jh
  * 2019年02月28日  15：34
+ *
+ * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+ *
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+ *
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ *
  */
 public class Q_066_PlusOne {
     public static int[] plusOne(int[] digits) {
@@ -29,7 +36,7 @@ public class Q_066_PlusOne {
 
 
     public static void main(String[] args) {
-        int[] arr = new int[]{8,9,9};
+        int[] arr = new int[]{9,9,9};
         for (int i:plusOne(arr)){
             System.out.print(i);
         }
