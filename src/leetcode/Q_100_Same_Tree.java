74558fd91674f1f9d842445244cedf4234fe45e7
jianghua2 <jianghua2@100tal.com>
2019-08-31 14:26:17 +0800

    相同的树

--- src/leetcode/Q_100_Same_Tree.java
+++ src/leetcode/Q_100_Same_Tree.java
@@ -1,22 +1,17 @@
 package leetcode;
 
 import sun.nio.cs.FastCharsetProvider;
+import util.TreeNode;
 
 /**
  * jh
  * 2019年03月06日  11：10
  *
+ * 给定两个二叉树，编写一个函数来检验它们是否相同。
+ *
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
  */
 public class Q_100_Same_Tree {
-
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode(int x) {
-            val = x;
-        }
-    }
 
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
@@ -28,9 +23,6 @@ public class Q_100_Same_Tree {
         }else {
             return false;
         }
-
-
-
     }
 
     public  StringBuilder preTree(TreeNode root,StringBuilder sb){
@@ -43,6 +35,20 @@ public class Q_100_Same_Tree {
         preTree(root.left,sb);
         preTree(root.right,sb);
         return sb;
+    }
+
+
+    //优化
+    public boolean isSameTree2(TreeNode p, TreeNode q) {
+        if(p==null && q == null){
+            return true;
+        }
+        if(p!=null && q!=null && p.val == q.val){
+            return isSameTree2(p.left,q.left) && isSameTree2(p.right,q.right);
+        }else {
+            return false;
+        }
+
     }
 }
 
