45a13fc6bff7c2464c868e83a752b555c03153cb
jianghua2 <jianghua2@100tal.com>
2019-10-11 19:42:47 +0800

    加油站

--- src/leetcode/Q_134_GasStation.java
+++ src/leetcode/Q_134_GasStation.java
@@ -20,55 +20,61 @@ package leetcode;
  * gas  = [1,2,3,4,5]
  * cost = [3,4,5,1,2]
  * 3
+ *
+ * 类似：最大自序和
+ * 烽火问题
  */
 public class Q_134_GasStation {
-    public static int canCompleteCircuit(int[] gas, int[] cost) {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        if(gas == null || cost == null || cost.length != gas.length){
+            return -1;
+        }
+        //当前油量
+        int tank = 0;
+        //历史油箱总量
+        int total = 0;
         int len = gas.length;
-        for (int i = 0; i < len; i++) {
-            int remain = 0;
-            int j = i;
-            for (int m = 0; m < len; m++) {
-                remain += gas[j];
-                if (remain < cost[j]) {
+        //分别查看以各个节点为头结点能否回到原点
+        for(int i = 0;i<gas.length;i++){
+            //从当前节点绕行一圈
+            tank = 0;
+            for(int j = 0;j<len;j++){
+                int cur = (j+i)%len;
+                tank += gas[cur]-cost[cur];
+                if(tank<0){
                     break;
-                } else {
-                    remain -= cost[j];
-                    j = (++j) % (len);
-
                 }
-                if (m == len - 1) {
+                if(j == len-1){
                     return i;
                 }
             }
+
         }
         return -1;
     }
 
-
+    //如果生产总量大于或等于消费总量，肯定有解
+    //从零开始遍历，如果剩余油量小于0，则从下一个开始，并记录开始位置，最后如果历史总量大于零则返回结果
     public int canCompleteCircuit2(int[] gas, int[] cost) {
-        if (gas == null || cost == null)
+        if(gas == null || cost == null || cost.length != gas.length){
             return -1;
+        }
 
-        int n = gas.length;
-
-
-        int netSum = 0;
+        //当前剩余量
         int curSum = 0;
-        int start = 0;
-        for (int i = 0; i < n; i++) {
-            netSum += gas[i] - cost[i];
-            curSum += gas[i] - cost[i];
-            if (curSum < 0) {
-                start = i + 1;
+        //从0开始，汽油量
+        int historySum = 0;
+        int result = 0;
+        for(int i = 0;i<gas.length;i++){
+            curSum += gas[i]-cost[i];
+            historySum += gas[i] - cost[i];
+            if(curSum <0){
+                result = i+1;
                 curSum = 0;
             }
         }
+        return historySum<0?-1:result;
 
-        if (netSum < 0) {
-            return -1;
-        } else {
-            return start;
-        }
     }
 
 
