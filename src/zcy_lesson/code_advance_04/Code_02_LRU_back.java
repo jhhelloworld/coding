c3f4811adf15ac65709a9a00e68592316ca9d823
jianghua2 <jianghua2@100tal.com>
2019-10-15 14:35:46 +0800

    加一

--- src/zcy_lesson/code_advance_04/Code_02_LRU_back.java
+++ src/zcy_lesson/code_advance_04/Code_02_LRU_back.java
@@ -13,9 +13,12 @@ public class Code_02_LRU_back<K,V>  {
 
     public Code_02_LRU_back(int capacity) {
         CAPACITY = capacity;
+        // 0.75为扩容因子，按照LeetCode题意，值应该为1
+        //accessOrder:true 按照更新顺序排序，false ：按照插入顺序
         map = new LinkedHashMap<K,V>(capacity,0.75f,true){
             @Override
             protected boolean removeEldestEntry(Map.Entry eldest) {
+                //当大于容量的时候删除最老元素
                 return size() > CAPACITY;
             }
         };
