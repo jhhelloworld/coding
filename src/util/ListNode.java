6ef6afd7a209d7e0d03f00730c24283c28e6b8e6
jianghua2 <jianghua2@100tal.com>
2019-10-29 16:01:43 +0800

    完善listNode  util方法

--- src/util/ListNode.java
+++ src/util/ListNode.java
@@ -1,5 +1,7 @@
 package util;
 
+import javax.print.attribute.standard.NumberUp;
+
 /**
  * jh
  * 2019年08月10日  13：18
@@ -11,4 +13,33 @@ public class ListNode {
     public ListNode(int x) {
         val = x;
     }
+
+
+
+    public  ListNode generateList(String s){
+        String[] strings = s.split(",");
+        if(strings.length == 0){
+            return null;
+        }
+        ListNode head = new ListNode(Integer.valueOf(strings[0]));
+        ListNode cur = head;
+        for(int i = 1;i<strings.length ;i++){
+            cur.next = new ListNode(Integer.valueOf(strings[i]));
+            cur = cur.next;
+        }
+        return head;
+    }
+
+
+    public  void  printList(ListNode head){
+        while (head!= null){
+            System.out.print(head.val+" ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+
+
+
 }
