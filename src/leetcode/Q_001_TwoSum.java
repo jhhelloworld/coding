5bfe564e72591757bf9a4c448089dabbdc05e35a
jianghua2 <jianghua2@100tal.com>
2020-01-04 12:26:45 +0800

    两数之和

--- src/leetcode/Q_001_TwoSum.java
+++ src/leetcode/Q_001_TwoSum.java
@@ -6,25 +6,21 @@ import java.util.Map;
 /**
  * jh
  * 2018年12月26日  19：12
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ *
  */
 public class Q_001_TwoSum {
 
-
     public int[] twoSum(int[] nums, int target) {
         Map<Integer,Integer> map = new HashMap<Integer,Integer>();
-        int[] result = new int[2];
         for(int i = 0;i<nums.length;i++){
-            
-
+            if(map.containsKey(target-nums[i])){
+                return new int[]{map.get(target-nums[i]),i};
+            }
+            map.put(nums[i],i);
         }
-
-
-
-
-
-        return nums;
-
-
+        return null;
     }
 
 }
