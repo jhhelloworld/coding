3796560d59550754a5d0794d2238c84c30f5d1dc
jhhelloworld <jh_helloworld@163.com>
2020-02-20 00:26:32 +0800

    蓄水池算法

--- src/zcy_book_v2/chapter_9_others/Problem_08_ReservoirSampling.java
+++ src/zcy_book_v2/chapter_9_others/Problem_08_ReservoirSampling.java
@@ -1,23 +1,27 @@
 package zcy_book_v2.chapter_9_others;
 
 public class Problem_08_ReservoirSampling {
-
-	// һ���򵥵��������������һ�����������ǲ���
-	public static int rand(int max) {
-		return (int) (Math.random() * max) + 1;
+	//产生从 1-k 的随机数
+	private static int rand(int k) {
+		return (int) (Math.random() * k) + 1;
 	}
 
 	public static int[] getKNumsRand(int k, int max) {
 		if (max < 1 || k < 1) {
 			return null;
 		}
+		//定义球袋
 		int[] res = new int[Math.min(k, max)];
-		for (int i = 0; i != res.length; i++) {
-			res[i] = i + 1; // ǰk����ֱ�ӽ�����
+		//第 1——k 个球 直接放入袋中
+		for (int i = 0; i < k; i++) {
+			res[i] = i + 1;
 		}
-		for (int i = k + 1; i < max + 1; i++) {
-			if (rand(i) <= k) { // ����i����������
-				res[rand(k) - 1] = i; // i�����������е�һ��
+		//处理第 k+1 —— N 个球
+		for (int i = k + 1; i <= max; i++) {
+			//这里表示 第 i 个球 进袋 的概率 为 k/i
+			if (rand(i) <= k) {
+				// i 随机替掉袋子中的一个
+				res[rand(k) - 1] = i;
 			}
 		}
 		return res;
