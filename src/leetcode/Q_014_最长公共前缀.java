5d00c528ecb4d8694e961ea2c4bf261d1238f501
jhhelloworld <jh_helloworld@163.com>
2020-07-09 20:29:12 +0800

    最长公共前缀

--- src/leetcode/Q_014_最长公共前缀.java
+++ src/leetcode/Q_014_最长公共前缀.java
@@ -3,12 +3,26 @@ package leetcode;
 /**
  * jh
  * 2019年08月15日  16：36
+ *编写一个函数来查找字符串数组中的最长公共前缀。
  *
- * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 如果不存在公共前缀，返回空字符串 ""。
  *
- * 如果不存在公共前缀，返回空字符串 ""。
+ * 示例 1:
+ *
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ * 示例 2:
+ *
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ * 说明:
+ *
+ * 所有输入只包含小写字母 a-z 。
+ *
+ *
  */
-public class Q_014_LongestCommonPrefix {
+public class Q_014_最长公共前缀 {
 
     public static String longestCommonPrefix(String[] strs) {
         StringBuilder sb = new StringBuilder();
