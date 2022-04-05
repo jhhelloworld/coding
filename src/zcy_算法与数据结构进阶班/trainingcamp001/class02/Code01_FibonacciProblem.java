4dedcfc135ba6b7b97ecc7af478551fbaea3b531
jh <jianghua07@meituan.com>
2022-04-05 21:55:28 +0800

    斐波那契数列

--- src/zcy_算法与数据结构进阶班/trainingcamp001/class02/Code01_FibonacciProblem.java
+++ src/zcy_算法与数据结构进阶班/trainingcamp001/class02/Code01_FibonacciProblem.java
@@ -2,228 +2,223 @@ package zcy_算法与数据结构进阶班.trainingcamp001.class02;
 
 
 /**
+ * 斐波那契数列矩阵乘法方式的实现
+ * 奶牛生小牛问题
+ * 第一年农场有1只成熟的母牛A，往后的每年：
+ * 1）每一只成熟的母牛都会生一只母牛
+ * 2）每一只新出生的母牛都在出生的第三年成熟
+ * 3）每一只母牛永远不会死
+ * 返回N年后牛的数量
+ * <p>
+ * F(N) = 1*F(N-1) + 0*F(N-2) + 1*F(N-3)
+ * 是一个三阶问题
+ * F(N),F(N-1),F(N-2) = ( F(3),F(2),F(3) ) * |3x3|^(n-3)
+ * 首先要求 这个 3x3 矩阵。
+ * 前几项： 1,2,3,4,6,9,13
+ * <p>
+ * 如果10年后牛会死
+ * F(N) = F(N-1) + F(n-3) -F(N-10)
+ * 是一个10阶矩阵
  *
 
-
- 斐波那契数列矩阵乘法方式的实现
-
-
-
-
-
-
-
- 奶牛生小牛问题
- 第一年农场有1只成熟的母牛A，往后的每年：
- 1）每一只成熟的母牛都会生一只母牛
- 2）每一只新出生的母牛都在出生的第三年成熟
- 3）每一只母牛永远不会死
- 返回N年后牛的数量
-
- F(N) = 1*F(N-1) + 0*F(N-2) + 1*F(N-3)
- 是一个三阶问题
- F(N),F(N-1),F(N-2) = ( F(3),F(2),F(3) ) * |3x3|^(n-3)
-首先要求 这个 3x3 矩阵。
-前几项： 1,2,3,4,6,9,13
-
- 如果10年后牛会死
- F(N) = F(N-1) + F(n-3) -F(N-10)
- 是一个10阶矩阵
-
-
-
- 台阶方法数问题
- 一个人可以一次往上迈1个台阶，也可以迈2个台阶，返回迈上N级台阶的方法数
- 2阶为题
- 和斐波那锲数列原理相同，只是初始项不同 1,2
- 因此单元矩阵也不一样
-
- 如果一个人可以迈1步，可以迈2步，可以迈5步
- 则是 F(N) = F(N-1) + F(N-5)
-
-
+ * 台阶方法数问题
+ * 一个人可以一次往上迈1个台阶，也可以迈2个台阶，返回迈上N级台阶的方法数
+ * 2阶为题
+ * 和斐波那锲数列原理相同，只是初始项不同 1,2
+ * 因此单元矩阵也不一样
+ * <p>
+ * 如果一个人可以迈1步，可以迈2步，可以迈5步
+ * 则是 F(N) = F(N-1) + F(N-2) + F(N-5)
  */
 public class Code01_FibonacciProblem {
 
-	public static int f1(int n) {
-		if (n < 1) {
-			return 0;
-		}
-		if (n == 1 || n == 2) {
-			return 1;
-		}
-		return f1(n - 1) + f1(n - 2);
-	}
+    public static int f1(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return f1(n - 1) + f1(n - 2);
+    }
 
-	public static int f2(int n) {
-		if (n < 1) {
-			return 0;
-		}
-		if (n == 1 || n == 2) {
-			return 1;
-		}
-		int res = 1;
-		int pre = 1;
-		int tmp = 0;
-		for (int i = 3; i <= n; i++) {
-			tmp = res;
-			res = res + pre;
-			pre = tmp;
-		}
-		return res;
-	}
+public static int f2(int n) {
+    if (n < 1) {
+        return 0;
+    }
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+    int res = 0;
+    int pre = 1;
+    int prepre = 1;
+    for (int i = 3; i <= n; i++) {
+        res = pre + prepre;
+        prepre = pre;
+        pre = res;
+    }
+    return res;
+}
 
-	public static int f3(int n) {
-		if (n < 1) {
-			return 0;
-		}
-		if (n == 1 || n == 2) {
-			return 1;
-		}
-		// [ 1 ,1 ]
-		// [ 1, 0 ]
-		int[][] base = { 
-				{ 1, 1 }, 
-				{ 1, 0 } 
-				};
-		int[][] res = matrixPower(base, n - 2);
-		return res[0][0] + res[1][0];
-	}
+    public static int f3(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        // [ 1 ,1 ]
+        // [ 1, 0 ]
+        int[][] base = {
+                {1, 1},
+                {1, 0}
+        };
+        int[][] res = matrixPower(base, n - 2);
+        return res[0][0] + res[1][0];
+    }
 
-	public static int[][] matrixPower(int[][] m, int p) {
-		int[][] res = new int[m.length][m[0].length];
-		for (int i = 0; i < res.length; i++) {
-			res[i][i] = 1;
-		}
-		
-		// res = 矩阵中的1
-		int[][] tmp = m;// 矩阵1次方
-		for (; p != 0; p >>= 1) {
-			if ((p & 1) != 0) {
-				res = muliMatrix(res, tmp);
-			}
-			tmp = muliMatrix(tmp, tmp);
-		}
-		return res;
-	}
+    // 先把p 转为二进制 比如 11 = 8+2+1
+    public static int[][] matrixPower(int[][] m, int p) {
+        int[][] res = new int[m.length][m.length];
+        //为什么需要这个单位矩阵，赋个初始值，其实这个相当于 x^0,
+        for (int i = 0; i < res.length; i++) {
+            res[i][i] = 1;
+        }
+        // res = 矩阵中的1
+        int[][] tmp = m;// 矩阵1次方
+        for (; p != 0; p >>= 1) {
+            if ((p & 1) != 0) {
+                res = muliMatrix(res, tmp);
+            }
+            tmp = muliMatrix(tmp, tmp);
+        }
+        return res;
+    }
 
-	// 两个矩阵乘完之后的结果返回
-	public static int[][] muliMatrix(int[][] m1, int[][] m2) {
-		int[][] res = new int[m1.length][m2[0].length];
-		for (int i = 0; i < m1.length; i++) {
-			for (int j = 0; j < m2[0].length; j++) {
-				for (int k = 0; k < m2.length; k++) {
-					res[i][j] += m1[i][k] * m2[k][j];
-				}
-			}
-		}
-		return res;
-	}
+    // 两个矩阵乘完之后的结果返回
+    public static int[][] muliMatrix(int[][] m1, int[][] m2) {
+        int[][] res = new int[m1.length][m2[0].length];
+        for (int i = 0; i < m1.length; i++) {
+            for (int j = 0; j < m2[0].length; j++) {
+                for (int k = 0; k < m2.length; k++) {
+                    res[i][j] += m1[i][k] * m2[k][j];
+                }
+            }
+        }
+        return res;
+    }
 
-	public static int s1(int n) {
-		if (n < 1) {
-			return 0;
-		}
-		if (n == 1 || n == 2) {
-			return n;
-		}
-		return s1(n - 1) + s1(n - 2);
-	}
+    //台阶问题
+    public static int s1(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return n;
+        }
+        return s1(n - 1) + s1(n - 2);
+    }
 
-	public static int s2(int n) {
-		if (n < 1) {
-			return 0;
-		}
-		if (n == 1 || n == 2) {
-			return n;
-		}
-		int res = 2;
-		int pre = 1;
-		int tmp = 0;
-		for (int i = 3; i <= n; i++) {
-			tmp = res;
-			res = res + pre;
-			pre = tmp;
-		}
-		return res;
-	}
+    public static int s2(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return n;
+        }
+        int res = 2;
+        int pre = 1;
+        int tmp = 0;
+        for (int i = 3; i <= n; i++) {
+            tmp = res;
+            res = res + pre;
+            pre = tmp;
+        }
+        return res;
+    }
 
-	public static int s3(int n) {
-		if (n < 1) {
-			return 0;
-		}
-		if (n == 1 || n == 2) {
-			return n;
-		}
-		int[][] base = { { 1, 1 }, { 1, 0 } };
-		int[][] res = matrixPower(base, n - 2);
-		return 2 * res[0][0] + res[1][0];
-	}
+    //基础矩阵与斐波那契数列相同
+    public static int s3(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return n;
+        }
+        int[][] base = {{1, 1}, {1, 0}};
+        int[][] res = matrixPower(base, n - 2);
+        return 2 * res[0][0] + res[1][0];
+    }
 
-	public static int c1(int n) {
-		if (n < 1) {
-			return 0;
-		}
-		if (n == 1 || n == 2 || n == 3) {
-			return n;
-		}
-		return c1(n - 1) + c1(n - 3);
-	}
 
-	public static int c2(int n) {
-		if (n < 1) {
-			return 0;
-		}
-		if (n == 1 || n == 2 || n == 3) {
-			return n;
-		}
-		int res = 3;
-		int pre = 2;
-		int prepre = 1;
-		int tmp1 = 0;
-		int tmp2 = 0;
-		for (int i = 4; i <= n; i++) {
-			tmp1 = res;
-			tmp2 = pre;
-			res = res + prepre;
-			pre = tmp1;
-			prepre = tmp2;
-		}
-		return res;
-	}
+    //奶牛问题
+    public static int c1(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1 || n == 2 || n == 3) {
+            return n;
+        }
+        return c1(n - 1) + c1(n - 3);
+    }
 
-	public static int c3(int n) {
-		if (n < 1) {
-			return 0;
-		}
-		if (n == 1 || n == 2 || n == 3) {
-			return n;
-		}
-		int[][] base = { 
-				{ 1, 1, 0 }, 
-				{ 0, 0, 1 }, 
-				{ 1, 0, 0 } };
-		int[][] res = matrixPower(base, n - 3);
-		return 3 * res[0][0] + 2 * res[1][0] + res[2][0];
-	}
+    public static int c2(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1 || n == 2 || n == 3) {
+            return n;
+        }
+        int res = 3;
+        int pre = 2;
+        int prepre = 1;
+        int tmp1 = 0;
+        int tmp2 = 0;
+        for (int i = 4; i <= n; i++) {
+            tmp1 = res;
+            tmp2 = pre;
+            res = res + prepre;
+            pre = tmp1;
+            prepre = tmp2;
+        }
+        return res;
+    }
 
-	public static void main(String[] args) {
-		int n = 19;
-		System.out.println(f1(n));
-		System.out.println(f2(n));
-		System.out.println(f3(n));
-		System.out.println("===");
+    public static int c3(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1 || n == 2 || n == 3) {
+            return n;
+        }
+        int[][] base = {
+                {1, 1, 0},
+                {0, 0, 1},
+                {1, 0, 0}};
+        int[][] res = matrixPower(base, n - 3);
+        return 3 * res[0][0] + 2 * res[1][0] + res[2][0];
+    }
 
-		System.out.println(s1(n));
-		System.out.println(s2(n));
-		System.out.println(s3(n));
-		System.out.println("===");
+    public static void main(String[] args) {
+        int n = 1;
+        System.out.println(f1(n));
+        System.out.println(f2(n));
+        System.out.println(f3(n));
+        System.out.println("===");
 
-		System.out.println(c1(n));
-		System.out.println(c2(n));
-		System.out.println(c3(n));
-		System.out.println("===");
+        System.out.println(s1(n));
+        System.out.println(s2(n));
+        System.out.println(s3(n));
+        System.out.println("===");
 
-	}
+        System.out.println(c1(n));
+        System.out.println(c2(n));
+        System.out.println(c3(n));
+        System.out.println("===");
+
+    }
+
+
+
 
 }
