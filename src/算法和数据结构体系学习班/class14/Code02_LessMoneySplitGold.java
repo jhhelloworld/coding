6a8117cf28b9409baa5f8670fb0b71875818724f
jh <jianghua07@meituan.com>
2022-04-07 10:23:24 +0800

    分金条问题

--- src/算法和数据结构体系学习班/class14/Code02_LessMoneySplitGold.java
+++ src/算法和数据结构体系学习班/class14/Code02_LessMoneySplitGold.java
@@ -2,6 +2,30 @@ package 算法和数据结构体系学习班.class14;
 
 import java.util.PriorityQueue;
 
+
+/**
+   一块金条切成两半，需要花费和长度值一样的铜板
+   比如长度为20的金条，无论怎么切都需要花费20个铜板。一群人想整分整块金条，怎么分最省铜板。
+   例如，给定数组:{10,20,30} 代表一共三个人，整块金条长度是60，金条要分成 10，20，30三部分
+   如果先把金条分成 10 和 50，花费60个铜板，再把长度为50的金条分成20和30，再花费50个铜板，一共花费110个铜板。
+   但是如果如果先分成30和30，在把其中一个30分成10和20，共花费90铜板
+   输入一个数组，返回分割的最小代价
+
+
+ 暴力递归思路
+ 合并任意两个元素，枚举所有可能。
+
+ 贪心思路：
+ 维护一个小根堆，存储当前金条状况。每次优先合并最小的两根金条。
+ 把合并后的金条重新放回小根堆，继续上面的步骤。
+
+
+ 堆和排序是贪心算法最常用的两个手段
+ 小根堆、大根堆、排序 是贪心算法最常用的三个模型。
+
+
+ */
+
 public class Code02_LessMoneySplitGold {
 
 	// 纯暴力！
@@ -16,9 +40,11 @@ public class Code02_LessMoneySplitGold {
 	// arr中只剩一个数字的时候，停止合并，返回最小的总代价
 	public static int process(int[] arr, int pre) {
 		if (arr.length == 1) {
+			//第一次coding 写成了返回是arr[0]，肯定不是arr[0] ，arr[0] 是固定值，即金条总长度。
 			return pre;
 		}
 		int ans = Integer.MAX_VALUE;
+		//枚举任意两段合并
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = i + 1; j < arr.length; j++) {
 				ans = Math.min(ans, process(copyAndMergeTwo(arr, i, j), pre + arr[i] + arr[j]));
@@ -27,6 +53,7 @@ public class Code02_LessMoneySplitGold {
 		return ans;
 	}
 
+	//返回一个新数组，数组长度比原数组小1，除i,j外其他元素不变，i+j合并成一个元素
 	public static int[] copyAndMergeTwo(int[] arr, int i, int j) {
 		int[] ans = new int[arr.length - 1];
 		int ansi = 0;
@@ -40,6 +67,7 @@ public class Code02_LessMoneySplitGold {
 	}
 
 	public static int lessMoney2(int[] arr) {
+		//默认按照整数大小排序
 		PriorityQueue<Integer> pQ = new PriorityQueue<>();
 		for (int i = 0; i < arr.length; i++) {
 			pQ.add(arr[i]);
@@ -47,10 +75,12 @@ public class Code02_LessMoneySplitGold {
 		int sum = 0;
 		int cur = 0;
 		while (pQ.size() > 1) {
+			//小根堆弹出两个，计算之后再扔回去。 其实这就是霍夫曼树
 			cur = pQ.poll() + pQ.poll();
 			sum += cur;
 			pQ.add(cur);
 		}
+		//堆里只剩一个的时候返回sum
 		return sum;
 	}
 
